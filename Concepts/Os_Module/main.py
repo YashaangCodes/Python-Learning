2bcbcb43bdfe304606182dc70c06c47e94202260
Yashaang Adhikari <237334096+YashaangCodes@users.noreply.github.com>
2026-05-29 13:17:26 +0530

    Practice working with os module paths

--- Concepts/Os_Module/main.py
+++ Concepts/Os_Module/main.py
@@ -1,7 +1,12 @@
 import os
 
-os.rmdir("Experiment")
+base_dir = os.getcwd()
+
+target_dir = os.path.join(base_dir,"Concepts","Os_Module")
+
+if os.getcwd() != target_dir:
+    os.chdir(target_dir)
+
+print(os.getcwd())
 
 
-
-    
