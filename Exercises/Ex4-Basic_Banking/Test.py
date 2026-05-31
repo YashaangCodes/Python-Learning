084b9f459642b7715113fd36fbfa6c44ec1feba1
Yashaang Adhikari <237334096+YashaangCodes@users.noreply.github.com>
2026-05-31 20:43:24 +0530

    Multiple errors are occuring in the action check and elif part

--- Exercises/Ex4-Basic_Banking/Test.py
+++ Exercises/Ex4-Basic_Banking/Test.py
@@ -1,11 +1,14 @@
-dic = {1 : "One", 2 : "Two", 3 : "Three"}
+dic = {"1" : "Create Account", "2" : "Deposit", "3" : "Withdraw", "4" : "Check Current Balance", "5" : "View Transaction History", "6" : "Quit"}
 print(dic.keys())
 print(dic.values())
 
-action = input("Enter : ")
-if action in dic.keys() or dic.values():
+action = input("Enter : ").strip().capitalize()
+if action not in dic.keys() and action not in dic.values():
+    print("No")
+else :
     print("Yes" , action)
 
-    
+# text = input("Enter: ").strip().capitalize()
 
+# print(text)
 
--- Exercises/Ex4-Basic_Banking/main.py
+++ Exercises/Ex4-Basic_Banking/main.py
@@ -18,6 +18,8 @@ def View_History():
 
 def Quit():
     print("Thank You for Trusting Us... Haave a Good Day")
+    return 1
+
 
 def Action_Window():
     para = """
@@ -31,26 +33,27 @@ def Action_Window():
     """
     print(para)
 
-    action = input("Either Enter the number or the word (EG - 1 OR Deposit) : ").strip().capitalize()
+    action = input("Either Enter the number or the word (EG - 2 OR Deposit) : ").strip().capitalize()
 
     action_dict = {"1" : "Create Account", "2" : "Deposit", "3" : "Withdraw", "4" : "Check Current Balance", "5" : "View Transaction History", "6" : "Quit"}
 
-    if not action in action_dict.keys() or action_dict.values():
+    if action not in action_dict.keys() and action not in action_dict.values():
         print("Invalid Action")
         Action_Window()
     
-    if action == "1" or "Create Account" :
+    if action == "1" or action == "Create Account" :
         Account_Setup()
-    elif action == "2" or "Deposit" :
+    elif action == "2" or action == "Deposit" :
         Deposit()
-    elif action == "3" or "Withdraw" :
+    elif action == "3" or action == "Withdraw" :
         Withdraw()
-    elif action == "4" or "Check Current Balance" :
+    elif action == "4" or action == "Check Current Balance" :
         Check_Balance()
-    elif action == "5" or "View Transaction History" :
+    elif action == "5" or action == "View Transaction History" :
         View_History()
     else :
         Quit()
-    
     return 1
 
+
+Action_Window()
