aef1586bbde26778396dc2451015893d8c7595aa
Yashaang Adhikari <237334096+YashaangCodes@users.noreply.github.com>
2026-05-31 20:03:42 +0530

    Finished with the basic framework of Account Setup and Action Window functions

--- Exercises/Ex4-Basic_Banking/Test.py
+++ Exercises/Ex4-Basic_Banking/Test.py
@@ -1,8 +1,11 @@
 dic = {1 : "One", 2 : "Two", 3 : "Three"}
 print(dic.keys())
 print(dic.values())
-if 2 in dic.keys():
-    print("Yes")
-if "Two" in dic.values():
-    print("Yes")
+
+action = input("Enter : ")
+if action in dic.keys() or dic.values():
+    print("Yes" , action)
+
+    
+
 
--- Exercises/Ex4-Basic_Banking/main.py
+++ Exercises/Ex4-Basic_Banking/main.py
@@ -2,15 +2,40 @@ def Account_Setup():
     global user_name, primay_balance
     user_name = input("Enter the Account holder's Name : ")
     primay_balance = float(input("Enter the Primary Balance : "))
-    Action()
+    Action_Window()
 
-def Action():
+def Action_Window():
     para = """
     What Action do you want to perform :
-    1] Deposit
-    2] Withdraw
-    3] Check current Balance
-    4] View Transaction History
+    1] Create Account
+    2] Deposit
+    3] Withdraw
+    4] Check Current Balance
+    5] View Transaction History
+    6] Quit
     """
-    print()
+    print(para)
+
+    action = input("Either Enter the number or the word (EG - 1 OR Deposit) : ").strip().capitalize()
+
+    action_dict = {"1" : "Create Account", "2" : "Deposit", "3" : "Withdraw", "4" : "Check Current Balance", "5" : "View Transaction History", "6" : "Quit"}
+
+    if not action in action_dict.keys() or action_dict.values():
+        print("Invalid Action")
+        Action_Window()
+    
+    if action == "1" or "Create Account" :
+        Account_Setup()
+    elif action == "2" or "Deposit" :
+        Deposit()
+    elif action == "3" or "Withdraw" :
+        Withdraw()
+    elif action == "4" or "Check Current Balance" :
+        Check_Balance()
+    elif action == "5" or "View Transaction History" :
+        View_History()
+    else :
+        Quit()
+    
+    return 1
 
