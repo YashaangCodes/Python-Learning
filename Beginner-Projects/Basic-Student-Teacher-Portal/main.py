37a61b1d2c573f60f8d2b0a56ff87b9e0f58e8bc
Yashaang Adhikari <237334096+YashaangCodes@users.noreply.github.com>
2026-06-04 16:07:54 +0530

    Initialized the setup_window with changing the directory if required and setting up the creation of required folders

--- Beginner-Projects/Basic-Student-Teacher-Portal/main.py
+++ Beginner-Projects/Basic-Student-Teacher-Portal/main.py
@@ -7,3 +7,26 @@ def file_path(base_dir, *arg):
 
     return target_dir
 
+def setup_window():
+
+    check_dir = os.path.join(os.getcwd(), "Beginner-Projects", "Basic-Student-Teacher-Portal")
+        
+    if os.path.isdir(check_dir):
+        os.chdir(check_dir)
+    
+    resource_path = file_path("Resources")
+
+    if not os.path.isdir(resource_path):
+        os.mkdir(resource_path)
+
+    teacher_path = file_path("Resources","Teacher")
+    student_path = file_path("Resources","Student")
+
+    if not os.path.isdir(teacher_path):
+        os.mkdir(teacher_path)
+    if not os.path.isdir(student_path) :
+        os.mkdir(student_path)
+
+
+setup_window()
+
