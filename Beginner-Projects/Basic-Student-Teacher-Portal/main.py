29697e3867581ee788ac7945c58ac7f3c66cea32
Yashaang Adhikari <237334096+YashaangCodes@users.noreply.github.com>
2026-06-05 13:51:59 +0530

    Completed the basic choice structure between log in and create

--- Beginner-Projects/Basic-Student-Teacher-Portal/main.py
+++ Beginner-Projects/Basic-Student-Teacher-Portal/main.py
@@ -7,6 +7,13 @@ def file_path(base_dir, *arg):
 
     return target_dir
 
+def sign_in():
+    print("You will be signed in")
+
+def create():
+    print("Account will be created")
+
+
 def setup_window():
 
     check_dir = os.path.join(os.getcwd(), "Beginner-Projects", "Basic-Student-Teacher-Portal")
@@ -26,6 +33,30 @@ def setup_window():
         os.mkdir(teacher_path)
     if not os.path.isdir(student_path) :
         os.mkdir(student_path)
+
+    sign_up_window()
+
+
+def sign_up_window():
+
+    text = """
+[1] Log in to an Existing Account
+[2] Create a New Account
+[3] Quit"""
+    print(text)
+
+    ac_dic = {"1" : sign_in , "2" : create}
+
+    while True:
+        action = input("Enter the corresponding Number : ")
+
+        if action in ac_dic:
+            ac_dic[action]()
+        elif action == "3":
+            print("You have quited")
+            break
+        else:
+            print("Invalid Input")
 
 
 if __name__=="__main__":
