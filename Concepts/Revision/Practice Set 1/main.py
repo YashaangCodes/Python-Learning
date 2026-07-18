071baa6fad50fbb5f6f4a407409debf3233cf650
Yashaang Adhikari <237334096+YashaangCodes@users.noreply.github.com>
2026-07-18 18:36:34 +0530

    Completed 6 basics questions so far... Really Basic ones

---
+++ Concepts/Revision/Practice Set 1/main.py
@@ -0,0 +1,38 @@
+print("\nQ1 :\n")
+
+print("Hello, World! Welcome to Python")
+
+print("\nQ2 :\n")
+
+print("Twinkle, twinkle, little star,\nHow I wonder what you are!")
+
+print("\nQ3 :\n")
+
+name = "Yashaang Adhikari"
+age = 19
+height = 167.3
+student = True
+
+print("Name :",name,"\nAge :", age, "\nHeight :",height,"\nStudent :",student)
+
+print("\nQ4 :\n")
+
+num  = "45"
+num = int(num) + 10
+print(num)
+
+print("\nQ5 :\n")
+
+fav = input("What is your favorite food? : ")
+print("Wow! I also like",fav)
+
+print("\nQ6 :\n")
+
+a = int(input("Enter number a : "))
+b = int(input("Enter number b : "))
+
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+
