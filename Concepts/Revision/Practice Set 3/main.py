8bb20236d112f105b90b77a0b040f5975fb7d637
Yashaang Adhikari <237334096+YashaangCodes@users.noreply.github.com>
2026-07-23 21:57:28 +0530

    Answered all the questions... Bonus questions remaining

--- Concepts/Revision/Practice Set 3/main.py
+++ Concepts/Revision/Practice Set 3/main.py
@@ -7,3 +7,61 @@ print(len(name))
 
 print("\nQ1.2 :\n")
 
+str1 = "Hello"
+str2 = "World"
+print(str1 + " " + str2)
+
+print("\nQ2.1 :\n")
+
+text = "Python Programming"
+print(text[:6])
+print(text[-6:])
+print(text[1::2])
+
+print("\nQ2.2 :\n")
+
+print(text[::-1])
+
+print("\nQ3.1 :\n")
+
+text = " i love python programming "
+text = text.strip()
+print(text)
+text = text.title()
+print(text)
+print(text.count("o"))
+
+print("\nQ3.2 :\n")
+
+text = "123abc"
+print(text.isalnum())
+
+print("\nQ4.1 :\n")
+
+sent = "My name is {} and I am {} years old"
+name = "John"
+age = 25
+print(sent.format(name,age))
+
+print("\nQ4.2 :\n")
+
+print(f"My name is {name} and I am {age} years old")
+
+print("\nQ5.1 :\n")
+
+sent = "Coding in Python is fun"
+sent = sent.replace("fun","awesome")
+print(sent)
+
+print("\nQ5.2 :\n")
+
+print(sent.index("Python"))
+
+print("\nQ5.3 :\n")
+
+print(sent.upper())
+
+print("\nQ6.1 :\n")
+
+
+
