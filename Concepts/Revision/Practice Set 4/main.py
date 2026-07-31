e0b6cc73d481cbcdc7c51c9dc094fd6b014794f4
Yashaang Adhikari <237334096+YashaangCodes@users.noreply.github.com>
2026-08-01 00:21:26 +0530

    Answered Question 1

---
+++ Concepts/Revision/Practice Set 4/main.py
@@ -0,0 +1,18 @@
+print("\nQ1.1 :\n")
+
+def greet():
+    print("Hello, Pyhton Learner!")
+
+greet()
+
+print("\nQ1.2 :\n")
+
+def square(num):
+    return num**2
+
+print(square(2))
+print(square(6))
+print(square(9))
+
+print("\nQ2.1 :\n")
+
