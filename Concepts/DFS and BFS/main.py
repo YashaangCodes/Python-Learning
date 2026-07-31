204c9ef0d1f446133094b030c47a0ace4652c91f
Yashaang Adhikari <237334096+YashaangCodes@users.noreply.github.com>
2026-07-31 18:19:34 +0530

    DFS Traversal success

--- Concepts/DFS and BFS/main.py
+++ Concepts/DFS and BFS/main.py
@@ -1,4 +1,11 @@
 
+def DFS(graph,vertex,visited,n):
+    visited[vertex] = True
+    print(vertex, end = " ")
+    for i in range(n):
+        if graph[vertex][i] == 1 and not visited[i] :
+            DFS(graph,i,visited,n)
+
 n = int(input("Enter the number of vertices : "))
 
 graph = []
@@ -8,8 +15,8 @@ for i in range(n):
     row = list(map(int, input().split()))
     graph.append(row)
 
-for row in graph:
-    print(*row)
+start = int(input("Enter the starting vertice : "))
 
-
-
+visited = [False] * n
+print("DFS Trversal : ", end="")
+DFS(graph,start,visited,n)
