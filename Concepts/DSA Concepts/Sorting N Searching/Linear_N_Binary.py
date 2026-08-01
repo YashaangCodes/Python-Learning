ee6d368976e555239c9429316795544869b3c168
Yashaang Adhikari <237334096+YashaangCodes@users.noreply.github.com>
2026-08-01 10:52:21 +0530

    Binary Search build complete

--- Concepts/DSA Concepts/Sorting N Searching/Linear_N_Binary.py
+++ Concepts/DSA Concepts/Sorting N Searching/Linear_N_Binary.py
@@ -4,8 +4,33 @@ def linear(data,target):
     for i in range(len(data)):
         if data[i] == target:
             print(f"Target {target} found at index {i}")
+            break
+    
+
+# Binary Search
+
+def binary(data,target):
+    data.sort()
+    print(f"Sorted Data : {data}")
+    low = 0
+    high = len(data)-1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if data[mid] == target :
+            print(f"Target {target} found at index {mid}")
+            break
+        elif data[mid] > target :
+            high = mid - 1
+        else :
+            low = mid + 1
+    else :
+        print("Target Not Found!")
+
 
 items = [2, 5, 7, 8, 9, 0, 6, 41, 23]
 mark = 0
 
 linear(items,mark)
+binary(items,mark)
+
